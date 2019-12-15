bbbf307edba3329926df9502d2db0ff7037005a2
nchen0 <nicky.chen0@gmail.com>
2019-12-15 11:36:41 -0500

    add ingredients

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { Route } from "react-router-dom";
 import AddRecipe from "./components/AddRecipe";
 import AuthContextProvider from "./contexts/AuthContext";
 import ViewMyRecipes from "./components/ViewMyRecipes";
+import ViewRecipe from "./components/ViewRecipe";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <RecipeContextProvider>
           <Navbar />
           <Route exact path="/" component={Recipes} />
+          <Route exact path="/recipe/:id" component={ViewRecipe} />
           <Route exact path="/my-recipes" component={MyRecipes} />
           <Route exact path="/my-recipe/:id" component={ViewMyRecipes} />
         </RecipeContextProvider>
--- client/src/components/ViewRecipe.js
+++ client/src/components/ViewRecipe.js
@@ -5,10 +5,16 @@ const ViewRecipe = () => {
   const { recipes } = useContext(RecipeContext);
   let id = Number(window.location.pathname.split("/")[2]);
   const recipe = recipes[id];
+  console.log("recipes is: ", recipes);
   return (
     <div>
-      <div>{recipe.name}</div>
-      <div>{recipe.ingredients}</div>
+      <img src={recipe.image} height="225px" width="300px" class="recipeImage" />
+
+      <div>{recipe.title}</div>
+      <h3>Ingredients</h3>
+      {recipe.extendedIngredients.map(ingredient => {
+        return <p>{ingredient.original}</p>;
+      })}
     </div>
   );
 };
