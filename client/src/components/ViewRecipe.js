538bf69ceaf05dac96d489e4983a1ec81729df13
nchen0 <nicky.chen0@gmail.com>
2019-12-15 11:50:43 -0500

    add directions

--- client/src/components/ViewRecipe.js
+++ client/src/components/ViewRecipe.js
@@ -6,6 +6,8 @@ const ViewRecipe = () => {
   let id = Number(window.location.pathname.split("/")[2]);
   const recipe = recipes[id];
   console.log("recipes is: ", recipes);
+  const instructions = recipe.instructions.split(".");
+  console.log("instructions is: ", instructions);
   return (
     <div>
       <img src={recipe.image} height="225px" width="300px" class="recipeImage" />
@@ -14,6 +16,10 @@ const ViewRecipe = () => {
       <h3>Ingredients</h3>
       {recipe.extendedIngredients.map(ingredient => {
         return <p>{ingredient.original}</p>;
+      })}
+      <h3>Directions</h3>
+      {instructions.map(instruction => {
+        return <p>{instruction.trim()}</p>;
       })}
     </div>
   );
