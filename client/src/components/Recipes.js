a6afdfd2ff38ef9d7a556a31f61d21a779a713f2
nchen0 <nicky.chen0@gmail.com>
2019-12-15 12:09:31 -0500

    change site name

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -7,14 +7,16 @@ import { Link } from "react-router-dom";
 const Recipes = props => {
   const { recipes, setRecipes } = useContext(RecipeContext);
   useEffect(() => {
-    axios
-      .get(
-        `https://cors-anywhere.herokuapp.com/${process.env.REACT_APP_API}/recipes/random?number=10&apiKey=${process.env.REACT_APP_RECIPESDB_APPKEY}`
-      )
-      .then(response => {
-        console.log("response is: ", response);
-        setRecipes(response.data.recipes);
-      });
+    if (!recipes.length) {
+      axios
+        .get(
+          `https://cors-anywhere.herokuapp.com/https://api.spoonacular.com/recipes/random?number=10&apiKey=${process.env.REACT_APP_RECIPESDB_APPKEY}`
+        )
+        .then(response => {
+          console.log("response is: ", response);
+          setRecipes(response.data.recipes);
+        });
+    }
   }, []);
 
   return (
