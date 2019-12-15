a689032465840454651c4aa1bd29885d2c432349
nchen0 <nicky.chen0@gmail.com>
2019-12-15 11:55:34 -0500

    refresh page

--- client/src/components/ViewRecipe.js
+++ client/src/components/ViewRecipe.js
@@ -1,26 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { RecipeContext } from "../contexts/RecipeContext";
+import axios from "axios";
 
 const ViewRecipe = () => {
-  const { recipes } = useContext(RecipeContext);
+  const { recipes, setRecipes } = useContext(RecipeContext);
+
+  // Take care of refreshing the page.
+  useEffect(() => {
+    if (!recipes.length) {
+      axios
+        .get(
+          `https://cors-anywhere.herokuapp.com/${process.env.REACT_APP_API}/recipes/random?number=10&apiKey=${process.env.REACT_APP_RECIPESDB_APPKEY}`
+        )
+        .then(response => {
+          setRecipes(response.data.recipes);
+        });
+    }
+  }, []);
   let id = Number(window.location.pathname.split("/")[2]);
   const recipe = recipes[id];
   console.log("recipes is: ", recipes);
-  const instructions = recipe.instructions.split(".");
-  console.log("instructions is: ", instructions);
+  let instructions = "";
+  if (recipe) {
+    instructions = recipe.instructions.split(".");
+  }
   return (
     <div>
-      <img src={recipe.image} height="225px" width="300px" class="recipeImage" />
+      {recipe ? (
+        <div>
+          <img src={recipe.image} height="225px" width="300px" class="recipeImage" />
 
-      <div>{recipe.title}</div>
-      <h3>Ingredients</h3>
-      {recipe.extendedIngredients.map(ingredient => {
-        return <p>{ingredient.original}</p>;
-      })}
-      <h3>Directions</h3>
-      {instructions.map(instruction => {
-        return <p>{instruction.trim()}</p>;
-      })}
+          <div>{recipe.title}</div>
+          <h3>Ingredients</h3>
+          {recipe.extendedIngredients.map(ingredient => {
+            return <p>{ingredient.original}</p>;
+          })}
+          <h3>Directions</h3>
+          {instructions.map(instruction => {
+            return <p>{instruction.trim()}</p>;
+          })}
+        </div>
+      ) : null}
     </div>
   );
 };
